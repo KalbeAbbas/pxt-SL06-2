802ea9cc36d6603994930d4b8e738aa91824405c
KalbeAbbas <kalbeabbas142@gmail.com>
2020-01-24 20:41:08 +0500

    Create SL06.cpp

---
+++ SL06.cpp
@@ -0,0 +1,33 @@
+#include "SL06.h"
+
+using namespace pxt;
+
+// function prototypes
+
+
+SL06Class::SL06Class()
+{
+}
+
+int SL06Class::delay(uint32_t time_ms)
+{
+#ifdef CODAL_I2C
+    sleep_ms(time_ms);
+#else
+    uBit.sleep(time_ms);
+#endif
+
+return 1;
+}
+
+namespace xSL06
+{
+static SL06Class *ptr = new SL06Class;
+    
+//%
+int delay(uint32_t time_ms)
+{
+  ptr->delay(time_ms);
+}
+} // namespace xSL06
+ 
